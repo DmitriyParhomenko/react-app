914a35d9175be06057a082e1e4d8ccbbe4991d3a
Dmitriy Parhomenko <dmitriyparhomenko140@gmail.com>
2020-05-30 09:19:18 +0300

    updated the navbar

--- client/src/Components/Header/Navigation.js
+++ client/src/Components/Header/Navigation.js
@@ -1,34 +1,72 @@
-import React, {Component} from 'react';
-import {Collapse, Nav, Navbar} from 'bootstrap-4-react';
+import React, {Component, useContext} from 'react';
+import {Collapse, Nav, Navbar, Button} from 'bootstrap-4-react';
 import {Link} from 'react-router-dom';
+import AuthService from '../../Context/AuthContext';
+import { AuthContext } from '../../Context/AuthContext';
 import Search from './Search';
 
-class Navigation extends Component {
-	render() {
-		return (
-			<Collapse navbar id="navbarSupportedContent">
-				<Navbar.Nav mr="auto">
-					<Nav.Item>
-						<Link className="nav-link" to="/">Home</Link>
-					</Nav.Item>
-					<Nav.Item>
-						<Link className="nav-link" to="/about">About us</Link>
-					</Nav.Item>
-					<Nav.Item>
-						<Link className="nav-link" to="/blog">Blog</Link>
-					</Nav.Item>
-					<Nav.Item>
-						<Link className="nav-link" to="/tracker">Tracker</Link>
-					</Nav.Item>
-					<Nav.Item>
-						<Link className="nav-link" to="/contacts">Contacts</Link>
-					</Nav.Item>
-				</Navbar.Nav>
+const Navigation = prop => {
+	const {isAuthenticated, user, setIsAuthenticated, setUser} = useContext(AuthContext);
 
-				<Search />
-			</Collapse>
+	const onClickLogoutHandler = () => {
+		AuthService.logout().then(data => {
+			if(data.success) {
+				setUser(data.user);
+				setIsAuthenticated(false);
+			}
+		});
+	};
+
+	const unauthenticatedNavBar = () => {
+		return (
+			<>
+				<Nav.Item>
+					<Link className="nav-link" to="/login">Login</Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Link className="nav-link" to="/register">Register</Link>
+				</Nav.Item>
+			</>
 		);
-	}
-}
+	};
+
+	const authenticatedNavBar = () => {
+		return (
+			<div>
+				<Link className="nav-link" to="/todos">Todos</Link>
+				{
+					user.role === 'admin' ?
+						<Link className="nav-link" to="/admin">Admin</Link> : null
+				}
+				<Button outline dark my="2 sm-0" onClick={onClickLogoutHandler}>Logout</Button>
+			</div>
+		);
+	};
+
+	return (
+		<Collapse navbar id="navbarSupportedContent">
+			<Navbar.Nav mr="auto">
+				<Nav.Item>
+					<Link className="nav-link" to="/">Home</Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Link className="nav-link" to="/about">About us</Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Link className="nav-link" to="/blog">Blog</Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Link className="nav-link" to="/tracker">Tracker</Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Link className="nav-link" to="/contacts">Contacts</Link>
+				</Nav.Item>
+				{ !isAuthenticated ? unauthenticatedNavBar() : authenticatedNavBar() }
+			</Navbar.Nav>
+
+			<Search />
+		</Collapse>
+	);
+};
 
 export default Navigation;
